bea9ca86e316fb1aa9a505f8b4a504f5769ab09d
WMayev <192962760+WMayev@users.noreply.github.com>
2025-01-11 11:41:35 +0100

    Add random message components

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const exercise = ['push-ups', 'squats', 'running', 'yoga', 'jumping rope'];
 const exerciseTime = [5, 10, 15, 30, 45];
 const results = ['more energy', 'better fitness', 'strong muscles', 'a healthy mind', 'improved physique'];
+
+const randomExercise = exercise[Math.floor(Math.random() * exercise.length)];
+const randomExerciseTime = exerciseTime[Math.floor(Math.random() * exerciseTime.length)];
+const randomResults = results[Math.floor(Math.random() * results.length)];
