274d50f64942db8f5e28b8265e399eb29d884e82
WMayev <192962760+WMayev@users.noreply.github.com>
2025-01-11 11:25:22 +0100

    Add three arrays of message components

---
+++ script.js
@@ -0,0 +1,3 @@
+const exercise = ['push-ups', 'squats', 'running', 'yoga', 'jumping rope'];
+const exerciseTime = [5, 10, 15, 30, 45];
+const results = ['more energy', 'better fitness', 'strong muscles', 'a healthy mind', 'improved physique'];
